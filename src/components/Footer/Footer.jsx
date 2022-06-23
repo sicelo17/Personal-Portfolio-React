ef0e507b94a9bbb605f3a29fca1d98f8e8c2628d
sicelo17 <sicelositsha@yahoo.com>
2022-06-23 14:08:01 +0200

    changed icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,13 +8,13 @@ import Gitub from "@iconscout/react-unicons/icons/uil-github";
 const Footer = () => {
   return (
     <div className="footer">
-      <img src={Wave} alt="" style={{ width: "100%" }} />
+      <img src={Wave} alt="" style={{ width: "100%", height:'13em' }} />
       <div className="f-content">
-        <span>Zainkeepscode@gmail.com</span>
+        <span>sicelositsha@yahoo.com</span>
         <div className="f-icons">
-          <Insta color="white" size={"3rem"} />
-          <Facebook color="white" size={"3rem"} />
-          <Gitub color="white" size={"3rem"} />
+          <Insta color="white" size={"2rem"} />
+          <Facebook color="white" size={"2rem"} />
+          <Gitub color="white" size={"2rem"} />
         </div>
       </div>
     </div>
